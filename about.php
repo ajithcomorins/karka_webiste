1daf12e803333850967a46cdf0d78b190142b78d
ajithcomorins <110983727+ajithcomorins@users.noreply.github.com>
2023-10-13 16:25:21 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/karka-favicon-1.png">
+    <link rel="icon" type="image/png" href="assets/img/karka-favicon-1.png">
+    <title>About Karka Academy | Pay After Placement Program</title>
+    <!--     Fonts and icons     -->
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200&display=swap" rel="stylesheet">
+    <!-- Nucleo Icons -->
+    <link href="assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <!-- Material Icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+    <!-- CSS Files -->
+    <link id="pagestyle" href="assets/css/material-kit-pro.css?v=3.0.3" rel="stylesheet" />
+    <link id="pagestyle" href="assets/css/style.css" rel="stylesheet" />
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-06CQLRKZ50"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-06CQLRKZ50');
+    </script>
+</head>
+<body>
+     <!-- header section start -->
+     <header class='about-us'>
+        <?php include 'include/header.php' ?>
+         <!-- You need to insert the Div-Tag with the ID "progressbar" -->
+         <div id="progressbar"></div>
+        <div class='about-bg'>
+            <img src='./assets/img/about-us.png' class='w-100'/>
+        </div>
+            <div class='about-title container'>
+                <div class='mx-auto text-center'>
+                    <h2>We aim to democratise the technology <br/>education for all</h2>
+                    <p class='sub-para p-0'>Since 2018, Karka academy is running intensive bootcamps in our learning labs to provide industry ready engineers to IT companies and make people take right career path.</p>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- heaader section end -->
+    <!-- section start  -->
+    <section class='about-us'>
+        <div class='about-solve'>
+            <div class='row m-0'>
+                <div class='col-lg-6 col-md-6 col-sm-12 d-flex align-items-stretch'>
+                    <div class="card card-contact card-raised shadow-xl mx-3 mx-md-4 mt-n7 mb-n2 about-card">
+                        <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent">
+                        <div class="bg-about shadow-primary border-radius-lg py-3 pe-1 mt-4">
+                            <h5 class="text-center text-white">PROBLEM</h5>
+                        </div>
+                        </div>
+                        <div class="card-body">
+                            <p>In our country, there are more and more youngsters who graduate each year. But companies are finding it really hard to identify right talents. Most of the students that graduate fall under either first generation graduates OR from not so-good socio-economic background
+                                <br/><br/>They really do not know how to identify & target appropriate job roles. They often end-up in jobs that don't deem fit for their ability. Also, in search of fortune they move to metro/tier-1 cities like Chennai, Bangalore etc., where they have to spend a lot of money in getting trained in appropriate training programmes and end up in jobs (and ofcourse spend for their survival until they get a job).</p>
+                        </div>
+                    </div>
+                </div>
+                <div class='col-lg-6 col-md-6 col-sm-12 d-flex align-items-stretch mt-sm-9 mt-md-0 mt-lg-0 mt-9 about-solution'>
+                    <div class="card card-contact card-raised shadow-xl mx-3 mx-md-4 mt-n7 about-card">
+                        <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent">
+                        <div class="bg-about shadow-primary border-radius-lg py-3 pe-1 mt-4">
+                            <h5 class="text-center text-white">SOLUTION</h5>
+                        </div>
+                        </div>
+                        <div class="card-body">
+                            <p>Hence there is a need to address this problem by providing support from industry practitioners to such graduates and make them industry ready. Such support from industry practitioners is not often available in tier-2, tier-3 cities. Especially in the Kanyakumari district that we operate.<br/></br/>Karka Academy acts as a high-lane bridge connecting them.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class='about-founder'>
+            <div class="container">
+              <h3 class="text-center mb-8">FOUNDERS</h3>
+              <div class="row device-width mx-auto">
+                <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+                  <div class="card card-profile">
+                    <div class="card-header mt-n4 mx-3 p-0 bg-transparent position-relative z-index-2">
+                      <a class="d-block blur-shadow-image">
+                        <img src="./assets/img/f-sateesh.jpg" alt="img-blur-shadow" class="img-fluid border-radius-lg w-100">
+                      </a>
+                    <div class="colored-shadow" style="background-image: url(&quot;../../assets/img/examples/card-profile1.jpg&quot;);"></div></div>
+                    <div class="card-body text-center">
+                        <h3>Sateesh kumar</h3>
+                        <p class='p-4'>15+ Years, Founder <br/>Ex-Yatra.com</p>
+                      <div class="row justify-content-center text-center">
+                        <div class="col-lg-12 col-12 text-center">
+                          <a href="https://www.linkedin.com/in/ssateeshk/">
+                            <img src='./assets/img/link.png' class=''/>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 col-12 mt-lg-0 mt-md-0 mt-sm-0 mt-5">
+                  <div class="card card-profile">
+                    <div class="card-header mt-n4 mx-3 p-0 bg-transparent position-relative z-index-2">
+                      <a class="d-block blur-shadow-image">
+                        <img src="./assets/img/f-ramesh.jpg" alt="img-blur-shadow" class="img-fluid border-radius-lg w-100">
+                      </a>
+                    <div class="colored-shadow" style="background-image: url(&quot;../../assets/img/examples/card-profile1.jpg&quot;);"></div></div>
+                    <div class="card-body text-center">
+                        <h3>Ramesh Varghese</h3>
+                        <p class='p-4'>11+ Years, Founder <br/>Ex-SMHaack</p>
+                      <div class="row justify-content-center text-center">
+                        <div class="col-lg-12 col-12 text-center">
+                          <a href="https://www.linkedin.com/in/ramesh-varghese/">
+                            <img src='./assets/img/link.png' class=''/>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+        <!-- address section start  -->
+      <div class="py-4 bg-white position-relative address-section">
+        <div class="page-header min-vh-50" style="background: linear-gradient(180deg, #202020 0%, #D587E4 100%);">
+          <span class="mask bg-gradient-dark opacity-4"></span>
+          <div class="container">
+            <div class="row justify-content-center">
+              <div class="col-lg-6 text-center mx-auto">
+                <h1 class="text-white mt-4 mb-1">Contact Us</h1>
+                <p class="lead text-white mb-6">We&#39;d like to talk more about what you need</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row mt-n5 blur border-radius-xl shadow-blur position-relative overflow-hidden">
+            <div class="col-lg-3 col-md-6 col-12 position-relative my-auto">
+              <div class="p-3 text-center">
+                <div class="icon icon-shape icon-md bg-color shadow-primary text-center mx-auto">
+                  <i class="material-icons opacity-10">menu_book</i>
+                </div>
+                <h5 class="mt-3 mb-1">Address</h5>
+                <p class="mb-0">4/5A, Putheri, Main road, Nagercoil</p>
+                
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12 position-relative my-auto">
+              <div class="p-3 text-center">
+                <div class="icon icon-shape icon-md bg-color shadow-primary text-center mx-auto">
+                  <i class="material-icons opacity-10">email</i>
+                </div>
+                <h5 class="mt-3 mb-1">Email</h5>
+                <p class="mb-0">hi@karka.academy</p>
+              </div>
+              
+            </div>
+            <div class="col-lg-3 col-md-6 col-12 position-relative my-auto">
+              <div class="p-3 text-center">
+                <div class="icon icon-shape icon-md bg-color shadow-primary text-center mx-auto">
+                  <i class="material-icons opacity-10">call</i>
+                </div>
+                <h5 class="mt-3 mb-1">Phone</h5>
+                <p class="mb-0">+91 94875 30243</p>
+              </div>
+              
+            </div>
+            <div class="col-lg-3 col-md-6 col-12 position-relative my-auto">
+              <div class="p-3 text-center">
+                <div class="icon icon-shape icon-md bg-color shadow-primary text-center mx-auto">
+                  <i class="material-icons opacity-10">contacts</i>
+                </div>
+                <h5 class="mt-3 mb-1">Contact</h5>
+                <p class="mb-0">www.karka.academy</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- address section end  -->
+         <!-- Back to top button -->
+         <button
+                type="button"
+                class="btn btn-danger btn-floating btn-lg"
+                id="btn-back-to-top"
+                >
+            <i class="fas fa-arrow-up"></i>
+        </button>
+        <!-- brochure pdf button  -->
+        <button
+          type="button"
+          class="btn btn-danger btn-floating btn-lg"
+          id="btn-brochure"
+          title='DOWNLOAD BROCHURE'
+          onclick="window.open('./assets/pdf/BROCHURE_KARKA .pdf')"
+        >
+         <a href='./assets/pdf/BROCHURE_KARKA .pdf' target='_blank'><i class="fa fa-file-pdf-o text-white"></i></a> 
+        </button>
+    </section>
+    <!-- section end  -->
+    <!-- footer section start  -->
+    <?php include 'include/footer.php' ?>
+    <!-- footer section end  -->
+</body>
+    <!--   Core JS Files   -->
+    <script src="assets/js/core/popper.min.js" type="text/javascript"></script>
+    <script src="assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+    <script src="assets/js/plugins/perfect-scrollbar.min.js"></script>
+    <!--  Plugin for TypedJS, full documentation here: https://github.com/mattboldt/typed.js/ -->
+    <script src="assets/js/plugins/typedjs.js"></script>
+    <script src="assets/js/plugins/choices.min.js"></script>
+    <script src="assets/js/plugins/flatpickr.min.js"></script>
+    <!--  Plugin for Parallax, full documentation here: https://github.com/wagerfield/parallax  -->
+    <script src="assets/js/plugins/parallax.min.js"></script>
+    <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+    <script src="assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+    <!--  Plugin for the blob animation -->
+    <script src="assets/js/plugins/anime.min.js" type="text/javascript"></script>
+    <!-- Control Center for Material UI Kit: parallax effects, scripts for the example pages etc -->
+    <!--  Google Maps Plugin    -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDTTfWur0PDbZWPr7Pmq8K3jiDp0_xUziI"></script>
+    <script src="assets/js/material-kit-pro.min.js?v=3.0.3" type="text/javascript"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
+     <!-- scroll top link  -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- main script js file  -->
+    <script src="./assets/js/main.js"></script>
+</html>
